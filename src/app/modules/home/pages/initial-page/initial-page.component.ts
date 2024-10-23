f4c83a81f3de67089956c67e224f56293ff416e4
GeniltonSilva <geniltonsilva4002@gmail.com>
2024-10-22 23:05:42 -0300

    graficos

--- src/app/modules/home/pages/initial-page/initial-page.component.ts
+++ src/app/modules/home/pages/initial-page/initial-page.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GraficosComponent } from "../../../../shared/components/graficos/graficos.component";
+import { PieChartComponent } from "../../../../shared/components/graficos/grafico-pie/grafico-pie.component";
 
 
 @Component({
   selector: 'app-initial-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GraficosComponent, PieChartComponent],
   templateUrl: './initial-page.component.html',
   styleUrl: './initial-page.component.scss',
 })
