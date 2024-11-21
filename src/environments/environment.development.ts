ca3ca6bc1e1f8c4b4b18043b8bb7b0c59ba344cc
aaglis <aglissilvafon@gmail.com>
2024-11-21 10:23:31 -0300

    trocando o texto renovar para um incone

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   env: "Local",
   apiUrl:
-    "https://5416-2804-248-f6bf-9100-d7a1-b1a8-4172-851c.ngrok-free.app/liv-api/",
+    "https://3744-2804-248-f6bf-9100-d7a1-b1a8-4172-851c.ngrok-free.app/liv-api/",
 };
