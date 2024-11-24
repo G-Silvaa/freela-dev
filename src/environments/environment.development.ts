b7fad8d1e1832b695bf227ad2bb1733300ea82bb
GeniltonSilva <geniltonsilva4002@gmail.com>
2024-11-24 14:04:20 -0300

    refactor/ removendo os comentarios

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   env: "Local",
   apiUrl:
-    "https://46d4-2804-248-f659-1a00-790c-53ce-9c2b-553c.ngrok-free.app/liv-api/",
+    "https://99b3-2804-248-f659-1a00-790c-53ce-9c2b-553c.ngrok-free.app/liv-api/",
 };
