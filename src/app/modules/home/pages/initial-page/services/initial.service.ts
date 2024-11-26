2fee330713ef779cdd9b83b175911903b9509ee3
geniltonsilva <geniltonsilva4002@gmail.com>
2024-11-26 14:48:38 -0300

    teste

---
+++ src/app/modules/home/pages/initial-page/services/initial.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraficosService {
+  private readonly API_URL = environment.apiUrl;
+
+  constructor(private readonly http: HttpClient) {}
+
+  getDados(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'ngrok-skip-browser-warning': '1',
+    });
+
+    return this.http.get(`${this.API_URL}domain/relatorio/query/recentes`, { headers });
+  }
+}
